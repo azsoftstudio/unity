67f5c38b078f49fd66b825c43b75dedb1da7b4d8
AZSoftStudio <105587585+azsoftstudio@users.noreply.github.com>
2025-10-05 13:58:04 +0600

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/unity/' // Replace with your GitHub repo name
+  base: '/' // Replace with your GitHub repo name
 }));
